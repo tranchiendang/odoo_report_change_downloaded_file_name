6a9559b11bfcddafe590135fae0a22edbf37d3a6
Chien <tranchiendang@gmail.com>
2015-07-23 13:25:15 +0700

    change qweb report file name

---
+++ __openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : "Utility Modules",
+    'version' : "1.0",
+    'author' : "tranchiendang@gmail.com",
+    'description' : '''
+        Change qweb downloaded file name, get report_file as name of downloaded file
+    ''',
+    'category' : "Utility",
+    'depends' : ['report'],
+    'website': 'http://chiendt.blogspot.com',
+    'data' : [],
+    'demo' : [],
+    'installable': True,
+    'auto_install': False
+}
